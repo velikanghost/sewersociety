721af729414597bb953f3594789ae3ac25f9d448
Chukwumdiebube <chukwumdiebubeojinta@gmail.com>
2023-02-21 15:51:01 +0100

    updates done

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,38 +1,24 @@
 import Image from "next/image"
 import Link from "next/link"
 import React from "react"
-import { FiTwitter } from "react-icons/fi"
-import { RxDiscordLogo } from "react-icons/rx"
-import Logo from "../public/img/sewer-logo.jpg"
+import Logo from "../public/img/sewer-s-crop.png"
 
 const Header = () => {
   return (
     <nav className="h-24 w-full shadow-nav z-40 fixed bg-[#F4F4EF] top-0">
       <div className="container mx-auto h-full flex justify-between items-center px-2 sm:px-0">
         <ul className="brand">
-          {/* <Link className="brand-link" href="/">
-            <span className="font-bold font-intro  text-[1.6rem] cursor-pointer align-middle uppercase text-[#5A5B59]">
-              Sewer Society
-            </span>
-          </Link> */}
-          <Link className="brand-link" href="/">
-            <Image src={Logo} alt="Logo" width={80} height={80} />
+          <Link className="brand-link max-w-[40px] relative" href="/">
+            <Image src={Logo} alt="Logo" className="" width={150} />
           </Link>
         </ul>
         <ul className="flex justify-center items-center sm:gap-[1.5rem] gap-[10px] sm:text-[1.65rem] text-[20px] text-[#333] cursor-pointer">
-          <Link className="brand-link" href="/about-us">
-            how to mint
-          </Link>
-          <Link className="brand-link" href="/sewer">
+          <Link className="brand-link btn btn-shine" href="/sewer">
             inscribe
           </Link>
-          {/* Socials */}
-          <a className="list-item" href="https://www.twitter.com/">
-            <FiTwitter />
-          </a>
-          <a className="list-item" href="https://discord.gg/">
-            <RxDiscordLogo />
-          </a>
+          <Link className="brand-link btn btn-shine" href="/about-us">
+            how to mint
+          </Link>
         </ul>
       </div>
     </nav>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image"
-import React from "react"
+import Image from "next/image";
+import { FiTwitter } from "react-icons/fi";
+import { RxDiscordLogo } from "react-icons/rx";
 
 const Hero = () => {
   return (
-    <section className="hero mt-[80px]">
+    <section className="hero mt-28">
       <div className="container mx-auto grid sm:grid-cols-2 justify-center items-center gap-[5rem]">
         <div className="w-full">
           <Image
@@ -18,17 +19,41 @@ const Hero = () => {
           <h3 className="font-bold font-intro text-4xl sm:text-[48px] pb-[12px] text-[#e85a4f] uppercase">
             Sewer <span className="text-[#333333]">Society</span>
           </h3>
-          <p className="text-base sm:text-[21px] text-[#8E8D89] text-justify">
+          <p className="text-base sm:text-[21px] text-[#5A5B59] text-justify">
             From the sewers we rise. 10000 strong, the 3rds represent a new wave
             and movement. Permanently inscribed on bitcoin using ordinals, sewer
             society is whatever you want(choose) it to be, a new world, a form
             of expression, a meme, a story passed down through generations,
             proof that you were here.
           </p>
+          {/* Socials */}
+          <div className="flex gap-2 mt-8">
+            {[
+              {
+                icon: FiTwitter,
+                path: "https://www.twitter.com/",
+                text: "Follow us",
+                color: "#00aaec"
+              },
+              {
+                icon: RxDiscordLogo,
+                path: "https://www.twitter.com/",
+                text: "Join us",
+                color: '#5865f2'
+              },
+            ].map(({ icon, path, text, color }, idx) => {
+              const Icon = icon;
+              return (
+                <a key={idx} className={`btn btn-shine flex items-center gap-2 text-base sm:text-[21px]`} href={path}>
+                  {text} <Icon color={color} />
+                </a>
+              );
+            })}
+          </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
