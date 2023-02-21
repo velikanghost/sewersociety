198977fb18fefb3a5ca9bcadceb729160882da24
velikanghost <velikanghost@yahoo.com>
2023-02-21 17:46:29 +0100

    inscribe page layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,8 +7,8 @@ import Logo from "../public/img/sewer-logo.jpg"
 
 const Header = () => {
   return (
-    <nav className="h-24 w-full shadow-nav z-40 fixed bg-[#F4F4EF] top-0">
-      <div className="container mx-auto h-full flex justify-between items-center px-2 sm:px-0">
+    <nav className="h-20 w-full shadow-nav z-40 fixed bg-[#F4F4EF] top-0">
+      <div className="container mx-auto h-full flex justify-between items-center px-6 xl:px-0">
         <ul className="brand">
           {/* <Link className="brand-link" href="/">
             <span className="font-bold font-intro  text-[1.6rem] cursor-pointer align-middle uppercase text-[#5A5B59]">
@@ -16,7 +16,13 @@ const Header = () => {
             </span>
           </Link> */}
           <Link className="brand-link" href="/">
-            <Image src={Logo} alt="Logo" width={130} height={100} />
+            <Image
+              className="h-[80px]"
+              src={Logo}
+              alt="Logo"
+              width={130}
+              height={80}
+            />
           </Link>
         </ul>
         <ul className="flex justify-center items-center sm:gap-[1.5rem] gap-[10px] sm:text-[1.65rem] text-[20px] text-[#333] cursor-pointer">
